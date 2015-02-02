5c3aedc23de6ef769303978ba221ed3dc83105e6
jntakpe <j.ntakpe@gmail.com>
2015-02-02 21:31:46 +0100

    Suppression de la classe de configuration des DataSource

--- src/main/java/com/github/jntakpe/qpq/config/QpqConfig.java
+++ src/main/java/com/github/jntakpe/qpq/config/QpqConfig.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Classe principale de configuration de l'application
@@ -18,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("com.github.jntakpe.qpq")
+@EntityScan("com.github.jntakpe.qpq.domain")
+@EnableJpaRepositories("com.github.jntakpe.qpq.repository")
 public class QpqConfig extends SpringBootServletInitializer {
 
     private static final Logger LOG = LoggerFactory.getLogger(QpqConfig.class);
