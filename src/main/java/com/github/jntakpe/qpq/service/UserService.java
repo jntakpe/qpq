94d75409623d2589d55f0d00b29b3667cd831af8
jntakpe <j.ntakpe@gmail.com>
2015-02-02 22:24:04 +0100

    Création de la classe de service pour l'entité User

---
+++ src/main/java/com/github/jntakpe/qpq/service/UserService.java
@@ -0,0 +1,51 @@
+package com.github.jntakpe.qpq.service;
+
+import com.github.jntakpe.qpq.domain.Authority;
+import com.github.jntakpe.qpq.domain.User;
+import com.github.jntakpe.qpq.repository.AuthorityRepository;
+import com.github.jntakpe.qpq.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+/**
+ * Services associés à l'entité {@link com.github.jntakpe.qpq.domain.User}
+ *
+ * @author jntakpe
+ */
+@Service
+public class UserService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    private static final int KEY_LENGTH = 20;
+
+    private UserRepository userRepository;
+
+    private AuthorityRepository authorityRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository, AuthorityRepository authorityRepository) {
+        this.userRepository = userRepository;
+        this.authorityRepository = authorityRepository;
+    }
+
+    public User create(User user) {
+        //FIXME encode password
+        addDefaultAuthorities(user);
+        user.setActivationKey(RandomStringUtils.randomAlphanumeric(KEY_LENGTH));
+        LOG.debug("Creating user {}", user);
+        return userRepository.save(user);
+    }
+
+    private void addDefaultAuthorities(User user) {
+        Authority roleUser = authorityRepository.findOne(Authority.ROLE_USER);
+        user.setAuthorities(new HashSet<>(1));
+        user.getAuthorities().add(roleUser);
+    }
+
+}
