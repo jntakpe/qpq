6760b371ee0ab85b934aaa81fd350a2cf40fe5cb
jntakpe <j.ntakpe@gmail.com>
2015-02-03 22:32:29 +0100

    Configuration de Spring Security sans OAuth2

---
+++ src/main/java/com/github/jntakpe/qpq/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.github.jntakpe.qpq.config;
+
+import com.github.jntakpe.qpq.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+/**
+ * Configuration de Spring Security
+ *
+ * @author jntakpe
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/dist/**/*.{js,css}");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+    private static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+    }
+}
---
+++ src/main/java/com/github/jntakpe/qpq/security/UserDetailsServiceImpl.java
@@ -0,0 +1,57 @@
+package com.github.jntakpe.qpq.security;
+
+import com.github.jntakpe.qpq.domain.User;
+import com.github.jntakpe.qpq.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Authentification Spring Security d'un utilisateur récupéré depuis la base de données
+ *
+ * @author jntakpe
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private static org.springframework.security.core.userdetails.User mapUserDetails(User user) {
+        //FIXME handle not activated user
+        List<GrantedAuthority> auths = mapAuthorities(user);
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), auths);
+    }
+
+    private static List<GrantedAuthority> mapAuthorities(User user) {
+        return user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        LOG.debug("Authenticating user {}", username);
+        return userRepository.findByLoginIgnoreCase(username)
+                .map(UserDetailsServiceImpl::mapUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found in DB"));
+    }
+}
