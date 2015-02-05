2f6e0236feafd38864396e8eec6bf7fa5af2ea4e
Jocelyn NTAKPE <jocelyn.ntakpe-prestataire@bforbank.com>
2015-02-05 17:12:44 +0100

    Utilisation de la conf Spring Boot par défaut pour Liquibase

--- src/main/java/com/github/jntakpe/qpq/config/DataSourceConfig.java
+++ src/main/java/com/github/jntakpe/qpq/config/DataSourceConfig.java
@@ -1,14 +1,8 @@
 package com.github.jntakpe.qpq.config;
 
-import liquibase.integration.spring.SpringLiquibase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import javax.sql.DataSource;
 
 /**
  * Configuration relative aux sources de données
@@ -20,17 +14,6 @@ import javax.sql.DataSource;
 @EnableJpaRepositories("com.github.jntakpe.qpq.repository")
 public class DataSourceConfig {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DataSourceConfig.class);
-
-    @Bean
-    public SpringLiquibase liquibase(DataSource dataSource) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog("classpath:liquibase/master.xml");
-        liquibase.setContexts("development, production");
-        LOG.debug("Configuring Liquibase");
-        return liquibase;
-    }
 }
 
 
