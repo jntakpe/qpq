b9eb7b7b6f9b6d19ba532f525281cdc4b63d1662
jntakpe <j.ntakpe@gmail.com>
2015-02-09 23:14:14 +0100

    Liens vers la page de login et swagger dans la page home

--- src/main/webapp/static/js/home/home.controller.js
+++ src/main/webapp/static/js/home/home.controller.js
@@ -1,10 +1,6 @@
 qpqApp.controller('HomeController', HomeController);
 
-function HomeController(Auth) {
+function HomeController() {
     "use strict";
 
-    var vm = this;
-    vm.test = function () {
-        Auth.login({username: 'jntakpe', password: 'password'});
-    };
 }
